4cf5b3f9a5389c1fdcc3396f27686d82317a8dca
Алтанбагана <altanbagana@mongolcontent.mn>
2023-11-28 17:54:42 +0800

    home

--- lib/features/Home/home.dart
+++ lib/features/Home/home.dart
@@ -1,6 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/src/widgets/container.dart';
-import 'package:flutter/src/widgets/framework.dart';
 
 class HomeScreen extends StatefulWidget {
   final Function layoutChange;
@@ -13,8 +11,57 @@ class HomeScreen extends StatefulWidget {
 class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
     return Scaffold(
-      body: Center(child: Text("HOME")),
+      backgroundColor: Color(0xFFF0F0F0),
+      body: Column(
+        children: [
+          SizedBox(
+            width: size.width,
+            height: size.height * 0.48,
+            child: Stack(
+              children: [
+                Image.asset("assets/img/Rectangle 28.png"),
+                Positioned(
+                  top: 80,
+                  left: -50,
+                  child: Image.asset(
+                    "assets/img/Ellipse 9.png",
+                    height: size.height * 0.15,
+                  ),
+                ),
+                Positioned(
+                  top: 50,
+                  left: 50,
+                  child: Image.asset(
+                    "assets/img/Ellipse 9.png",
+                    height: size.height * 0.1,
+                  ),
+                ),
+                Positioned(
+                  top: 0,
+                  left: 90,
+                  child: Image.asset(
+                    "assets/img/Ellipse 9.png",
+                    height: size.height * 0.08,
+                  ),
+                ),
+                Row(
+                  children: [
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text("Өглөөний мэнд?"),
+                        Text("Tushig"),
+                      ],
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
--- lib/features/Layout/layout.dart
+++ lib/features/Layout/layout.dart
@@ -1,5 +1,3 @@
-import 'dart:developer';
-
 import 'package:flutter/material.dart';
 import 'package:wallet_app/core/utils/bnb_custom_painter.dart';
 import 'package:wallet_app/core/utils/colors.dart';
